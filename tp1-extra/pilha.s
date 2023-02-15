65c27a18e2addbc11c2b72a79addffc87f18ecb6
pedro <pedro@Ubuntu.myguest.virtualbox.org>
2023-02-15 13:03:38 -0300

    name change - loop

--- tp1-extra/pilha.s
+++ tp1-extra/pilha.s
@@ -25,29 +25,84 @@ _MemMgr_COLLECTOR:
 _MemMgr_TEST:
 	.word	0
 	.word	-1
-str_const9:
+str_const15:
+	.word	5
+	.word	6
+	.word	String_dispTab
+	.word	int_const1
+	.ascii	"Compare"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const14:
+	.word	5
+	.word	6
+	.word	String_dispTab
+	.word	int_const2
+	.ascii	"String"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const13:
+	.word	5
+	.word	6
+	.word	String_dispTab
+	.word	int_const3
+	.ascii	"Bool"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const12:
 	.word	5
 	.word	5
 	.word	String_dispTab
-	.word	int_const0
+	.word	int_const4
+	.ascii	"Int"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const11:
+	.word	5
+	.word	6
+	.word	String_dispTab
+	.word	int_const3
+	.ascii	"Main"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const10:
+	.word	5
+	.word	5
+	.word	String_dispTab
+	.word	int_const5
+	.ascii	"IO"
+	.byte	0	
+	.align	2
+	.word	-1
+str_const9:
+	.word	5
+	.word	6
+	.word	String_dispTab
+	.word	int_const2
+	.ascii	"Object"
 	.byte	0	
 	.align	2
 	.word	-1
 str_const8:
 	.word	5
-	.word	6
+	.word	8
 	.word	String_dispTab
-	.word	int_const1
-	.ascii	"String"
+	.word	int_const6
+	.ascii	"<basic class>"
 	.byte	0	
 	.align	2
 	.word	-1
 str_const7:
 	.word	5
-	.word	6
+	.word	5
 	.word	String_dispTab
-	.word	int_const2
-	.ascii	"Bool"
+	.word	int_const7
+	.ascii	"\n"
 	.byte	0	
 	.align	2
 	.word	-1
@@ -55,17 +110,16 @@ str_const6:
 	.word	5
 	.word	5
 	.word	String_dispTab
-	.word	int_const3
-	.ascii	"Int"
+	.word	int_const0
 	.byte	0	
 	.align	2
 	.word	-1
 str_const5:
 	.word	5
-	.word	6
+	.word	7
 	.word	String_dispTab
-	.word	int_const2
-	.ascii	"Main"
+	.word	int_const8
+	.ascii	"pilha.cl"
 	.byte	0	
 	.align	2
 	.word	-1
@@ -73,88 +127,94 @@ str_const4:
 	.word	5
 	.word	5
 	.word	String_dispTab
-	.word	int_const4
-	.ascii	"IO"
+	.word	int_const7
+	.ascii	"x"
 	.byte	0	
 	.align	2
 	.word	-1
 str_const3:
 	.word	5
-	.word	6
+	.word	5
 	.word	String_dispTab
-	.word	int_const1
-	.ascii	"Object"
+	.word	int_const7
+	.ascii	"d"
 	.byte	0	
 	.align	2
 	.word	-1
 str_const2:
 	.word	5
-	.word	8
+	.word	5
 	.word	String_dispTab
-	.word	int_const5
-	.ascii	"<basic class>"
+	.word	int_const7
+	.ascii	"e"
 	.byte	0	
 	.align	2
 	.word	-1
 str_const1:
 	.word	5
-	.word	7
+	.word	5
 	.word	String_dispTab
-	.word	int_const6
-	.ascii	"pilha.cl"
+	.word	int_const7
+	.ascii	"s"
 	.byte	0	
 	.align	2
 	.word	-1
 str_const0:
 	.word	5
-	.word	8
+	.word	5
 	.word	String_dispTab
 	.word	int_const7
-	.ascii	"Hello, world!\n"
+	.ascii	"+"
 	.byte	0	
 	.align	2
+	.word	-1
+int_const8:
+	.word	3
+	.word	4
+	.word	Int_dispTab
+	.word	8
 	.word	-1
 int_const7:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	14
+	.word	1
 	.word	-1
 int_const6:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	8
+	.word	13
 	.word	-1
 int_const5:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	13
+	.word	2
 	.word	-1
 int_const4:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	2
+	.word	3
 	.word	-1
 int_const3:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	3
+	.word	4
 	.word	-1
 int_const2:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	4
+	.word	6
 	.word	-1
 int_const1:
 	.word	3
 	.word	4
 	.word	Int_dispTab
-	.word	6
+	.word	7
 	.word	-1
 int_const0:
 	.word	3
@@ -174,12 +234,13 @@ bool_const1:
 	.word	Bool_dispTab
 	.word	1
 class_nameTab:
-	.word	str_const3
-	.word	str_const4
-	.word	str_const5
-	.word	str_const6
-	.word	str_const7
-	.word	str_const8
+	.word	str_const9
+	.word	str_const10
+	.word	str_const11
+	.word	str_const12
+	.word	str_const13
+	.word	str_const14
+	.word	str_const15
 class_objTab:
 	.word	Object_protObj
 	.word	Object_init
@@ -193,10 +254,21 @@ class_objTab:
 	.word	Bool_init
 	.word	String_protObj
 	.word	String_init
+	.word	Compare_protObj
+	.word	Compare_init
 Object_dispTab:
 	.word	Object.abort
 	.word	Object.type_name
 	.word	Object.copy
+Compare_dispTab:
+	.word	Object.abort
+	.word	Object.type_name
+	.word	Object.copy
+	.word	Compare.isPlus
+	.word	Compare.isS
+	.word	Compare.isE
+	.word	Compare.isD
+	.word	Compare.isX
 String_dispTab:
 	.word	Object.abort
 	.word	Object.type_name
@@ -235,6 +307,11 @@ Object_protObj:
 	.word	3
 	.word	Object_dispTab
 	.word	-1
+Compare_protObj:
+	.word	6
+	.word	3
+	.word	Compare_dispTab
+	.word	-1
 String_protObj:
 	.word	5
 	.word	5
@@ -261,8 +338,10 @@ IO_protObj:
 	.word	-1
 Main_protObj:
 	.word	2
-	.word	3
+	.word	5
 	.word	Main_dispTab
+	.word	0
+	.word	str_const6
 	.globl	heap_start
 heap_start:
 	.word	0
@@ -279,6 +358,20 @@ Object_init:
 	sw	$ra 4($sp)
 	addiu	$fp $sp 4
 	move	$s0 $a0
+	move	$a0 $s0
+	lw	$fp 12($sp)
+	lw	$s0 8($sp)
+	lw	$ra 4($sp)
+	addiu	$sp $sp 12
+	jr	$ra	
+Compare_init:
+	addiu	$sp $sp -12
+	sw	$fp 12($sp)
+	sw	$s0 8($sp)
+	sw	$ra 4($sp)
+	addiu	$fp $sp 4
+	move	$s0 $a0
+	jal	Object_init
 	move	$a0 $s0
 	lw	$fp 12($sp)
 	lw	$s0 8($sp)
@@ -349,11 +442,127 @@ Main_init:
 	addiu	$fp $sp 4
 	move	$s0 $a0
 	jal	IO_init
+	la	$a0 Compare_protObj
+	jal	Object.copy
+	jal	Compare_init
+	sw	$a0 12($s0)
+	la	$a0 str_const6
+	sw	$a0 16($s0)
 	move	$a0 $s0
 	lw	$fp 12($sp)
 	lw	$s0 8($sp)
 	lw	$ra 4($sp)
 	addiu	$sp $sp 12
+	jr	$ra	
+Compare.isPlus:
+	addiu	$sp $sp -16
+	sw	$fp 16($sp)
+	sw	$s0 12($sp)
+	sw	$ra 8($sp)
+	addiu	$fp $sp 4
+	move	$s0 $a0
+	sw	$s1 0($fp)
+	lw	$s1 16($fp)
+	la	$t2 str_const0
+	move	$t1 $s1
+	la	$a0 bool_const1
+	beq	$t1 $t2 label0
+	la	$a1 bool_const0
+	jal	equality_test
+label0:
+	lw	$s1 0($fp)
+	lw	$fp 16($sp)
+	lw	$s0 12($sp)
+	lw	$ra 8($sp)
+	addiu	$sp $sp 20
+	jr	$ra	
+Compare.isS:
+	addiu	$sp $sp -16
+	sw	$fp 16($sp)
+	sw	$s0 12($sp)
+	sw	$ra 8($sp)
+	addiu	$fp $sp 4
+	move	$s0 $a0
+	sw	$s1 0($fp)
+	lw	$s1 16($fp)
+	la	$t2 str_const1
+	move	$t1 $s1
+	la	$a0 bool_const1
+	beq	$t1 $t2 label1
+	la	$a1 bool_const0
+	jal	equality_test
+label1:
+	lw	$s1 0($fp)
+	lw	$fp 16($sp)
+	lw	$s0 12($sp)
+	lw	$ra 8($sp)
+	addiu	$sp $sp 20
+	jr	$ra	
+Compare.isE:
+	addiu	$sp $sp -16
+	sw	$fp 16($sp)
+	sw	$s0 12($sp)
+	sw	$ra 8($sp)
+	addiu	$fp $sp 4
+	move	$s0 $a0
+	sw	$s1 0($fp)
+	lw	$s1 16($fp)
+	la	$t2 str_const2
+	move	$t1 $s1
+	la	$a0 bool_const1
+	beq	$t1 $t2 label2
+	la	$a1 bool_const0
+	jal	equality_test
+label2:
+	lw	$s1 0($fp)
+	lw	$fp 16($sp)
+	lw	$s0 12($sp)
+	lw	$ra 8($sp)
+	addiu	$sp $sp 20
+	jr	$ra	
+Compare.isD:
+	addiu	$sp $sp -16
+	sw	$fp 16($sp)
+	sw	$s0 12($sp)
+	sw	$ra 8($sp)
+	addiu	$fp $sp 4
+	move	$s0 $a0
+	sw	$s1 0($fp)
+	lw	$s1 16($fp)
+	la	$t2 str_const3
+	move	$t1 $s1
+	la	$a0 bool_const1
+	beq	$t1 $t2 label3
+	la	$a1 bool_const0
+	jal	equality_test
+label3:
+	lw	$s1 0($fp)
+	lw	$fp 16($sp)
+	lw	$s0 12($sp)
+	lw	$ra 8($sp)
+	addiu	$sp $sp 20
+	jr	$ra	
+Compare.isX:
+	addiu	$sp $sp -16
+	sw	$fp 16($sp)
+	sw	$s0 12($sp)
+	sw	$ra 8($sp)
+	addiu	$fp $sp 4
+	move	$s0 $a0
+	sw	$s1 0($fp)
+	lw	$s1 16($fp)
+	la	$t2 str_const4
+	move	$t1 $s1
+	la	$a0 bool_const1
+	beq	$t1 $t2 label4
+	la	$a1 bool_const0
+	jal	equality_test
+label4:
+	lw	$s1 0($fp)
+	lw	$fp 16($sp)
+	lw	$s0 12($sp)
+	lw	$ra 8($sp)
+	addiu	$sp $sp 20
 	jr	$ra	
 Main.main:
 	addiu	$sp $sp -12
@@ -362,18 +571,62 @@ Main.main:
 	sw	$ra 4($sp)
 	addiu	$fp $sp 4
 	move	$s0 $a0
-	la	$a0 str_const0
+label5:
+	lw	$a0 16($s0)
+	sw	$a0 0($sp)
+	addiu	$sp $sp -4
+	lw	$a0 12($s0)
+	bne	$a0 $zero label8
+	la	$a0 str_const5
+	li	$t1 24
+	jal	_dispatch_abort
+label8:
+	lw	$t1 8($a0)
+	lw	$t1 28($t1)
+	jalr		$t1
+	lw	$t1 12($a0)
+	la	$a0 bool_const1
+	beqz	$t1 label7
+	la	$a0 bool_const0
+label7:
+	lw	$t1 12($a0)
+	beq	$t1 $zero label6
+	move	$a0 $s0
+	bne	$a0 $zero label9
+	la	$a0 str_const5
+	li	$t1 25
+	jal	_dispatch_abort
+label9:
+	lw	$t1 8($a0)
+	lw	$t1 20($t1)
+	jalr		$t1
+	sw	$a0 16($s0)
+	la	$a0 str_const7
+	sw	$a0 0($sp)
+	addiu	$sp $sp -4
+	lw	$a0 16($s0)
+	bne	$a0 $zero label10
+	la	$a0 str_const5
+	li	$t1 26
+	jal	_dispatch_abort
+label10:
+	lw	$t1 8($a0)
+	lw	$t1 16($t1)
+	jalr		$t1
 	sw	$a0 0($sp)
 	addiu	$sp $sp -4
 	move	$a0 $s0
-	bne	$a0 $zero label0
-	la	$a0 str_const1
-	li	$t1 3
+	bne	$a0 $zero label11
+	la	$a0 str_const5
+	li	$t1 26
 	jal	_dispatch_abort
-label0:
+label11:
 	lw	$t1 8($a0)
 	lw	$t1 12($t1)
 	jalr		$t1
+	b	label5
+label6:
+	move	$a0 $zero
 	lw	$fp 12($sp)
 	lw	$s0 8($sp)
 	lw	$ra 4($sp)
--- tp1-extra/stack.cl
+++ tp1-extra/stack.cl
@@ -1,5 +1,30 @@
-class Main inherits IO {
-  main() : Object {
-    out_string("Hello, world!\n")
+class Compare{
+  isPlus(s : String) : Bool {
+    s = "+"
   };
+  isS(s : String) : Bool {
+    s = "s"
+  };
+  isE(s : String) : Bool {
+    s = "e"
+  };
+  isD(s : String) : Bool {
+    s = "d"
+  };
+  isX(s : String) : Bool {
+    s = "x"
+  };
+};
+
+class Main inherits IO {
+  compare : Compare <- new Compare;
+  input : String <- "";
+
+  main() : Object {{
+      while (not compare.isX(input)) loop {
+        input <- in_string();
+        out_string(input.concat("\n"));
+      }
+      pool;
+  }};
 };  
